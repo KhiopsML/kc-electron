5fed746e3c1b9e5332b370a5f9d714630189c1ef
Stéphane Bouget <stephane.bouget@orange.com>
2024-11-27 18:09:58 +0100

    Add TRACKER_ID property to environment configuration files

--- src/environments/environment.web.prod.ts
+++ src/environments/environment.web.prod.ts
@@ -1,4 +1,5 @@
 export const APP_CONFIG = {
   production: true,
   environment: 'WEB-PROD',
+  TRACKER_ID: '',
 };
--- src/environments/environment.web.ts
+++ src/environments/environment.web.ts
@@ -1,4 +1,5 @@
 export const APP_CONFIG = {
   production: false,
   environment: 'WEB',
+  TRACKER_ID: '',
 };
