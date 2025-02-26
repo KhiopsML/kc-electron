5c15e4714eb8799c0f1972bf369d2700893a027c
Stéphane Bouget <stephane.bouget@orange.com>
2025-02-26 11:27:57 +0100

    Add hot reload functionality on khiops lib direct development

--- scripts/build-dev-watch.sh
+++ scripts/build-dev-watch.sh
@@ -6,9 +6,11 @@ ANGULAR_JSON="angular.json"
 # Update the angular.json file to replace the script path
 sed -i 's|"node_modules/khiops-visualization/khiops-webcomponents.bundle.js"|"../visualization-component/dist/khiops-webcomponent/main.js"|g' $ANGULAR_JSON
 
-# Start the application
-yarn start 
-
 # Change directory to visualization component and build development webcomponents
 cd ../visualization-component
-yarn buildDev:webcomponents
+yarn buildDev:webcomponents &
+
+# Start the application
+cd ../khiops-covisualization-electron
+sleep 20
+yarn start 
