29ce5546933d622111164539a7345bd9805c7c1f
Stéphane Bouget <stephane.bouget@orange.com>
2025-04-24 09:56:40 +0200

    Update lib versions

--- app/main.ts
+++ app/main.ts
@@ -9,6 +9,7 @@ const log = require('electron-log');
 import { machineIdSync } from 'node-machine-id';
 import * as url from 'url';
 const storage = require('electron-json-storage');
+import debug from 'electron-debug';
 
 let win: BrowserWindow | null = null;
 const args = process.argv.slice(1),
@@ -100,7 +101,6 @@ function createWindow(): BrowserWindow {
   // win.webContents.openDevTools();
 
   if (serve) {
-    const debug = require('electron-debug');
     debug();
 
     require('electron-reloader')(module);
